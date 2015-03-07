149a73660995606cece5f7cb5e6b175cf5c088df
MM1314 <1056426706@qq.com>
2015-03-07 20:22:00 +0800

    用户管理服务、BL整理

--- SOA.WLIMS/SOA.WLIMS.BLL/UserBLL.cs
+++ SOA.WLIMS/SOA.WLIMS.BLL/UserBLL.cs
@@ -3,55 +3,147 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using SOA.WLIMS.Contract;
+using SOA.WLIMS.Models;
+using System.Web.Security;
+using SOA.WLIMS.DAL;
 
 namespace SOA.WLIMS.BLL
 {
     public class UserBLL:IUserService
     {
+        SOAWLDBEntities DB = new SOAWLDBEntities();
+
+
         public int MinPasswordLength()
         {
-            throw new NotImplementedException();
+            return 8;
+        }
+
+
+        public List<UserModel> Query(QueryParam param)
+        {
+            return DB.User.Select(model => new UserModel()
+            {
+                ID = model.ID,
+                Name = model.Name,
+                AliasName = model.AliasName,
+                Email = model.Email,
+                Password = model.Password,
+                Role = model.Role,
+                EmployeCode = model.EmployeCode,
+                Enable = model.Enable,
+                IsDeleted = model.IsDeleted
+            }).ToList();
         }
 
         public bool ValidateUser(string userName, string password)
         {
-            throw new NotImplementedException();
+            return DB.User.Where(o => o.Name == userName.Trim() && o.Password == password.Trim()).Count() == 1;
         }
 
-        public System.Web.Security.MembershipCreateStatus CreateUser(SOA.WLIMS.Models.RegisterModel model)
+        public MembershipCreateStatus CreateUser(RegisterModel model)
         {
-            throw new NotImplementedException();
+            try
+            {
+                DB.User.AddObject(new User()
+                {
+                    Name = model.UserName,
+                    AliasName = model.UserName,
+                    Email = model.Email,
+                    Password = model.Password,
+                    Role = model.Role,
+                    EmployeCode = model.EmployeCode,
+                    Enable = true,
+                    IsDeleted = false
+                });
+                DB.SaveChanges();
+                return System.Web.Security.MembershipCreateStatus.Success;
+            }
+            catch (Exception ex)
+            {
+                return System.Web.Security.MembershipCreateStatus.ProviderError;
+            }
         }
 
         public bool ChangePassword(string userName, string oldPassword, string newPassword)
         {
-            throw new NotImplementedException();
+            if (String.IsNullOrEmpty(userName)) throw new ArgumentException("值不能为 null 或为空。", "userName");
+            if (String.IsNullOrEmpty(oldPassword)) throw new ArgumentException("值不能为 null 或为空。", "oldPassword");
+            if (String.IsNullOrEmpty(newPassword)) throw new ArgumentException("值不能为 null 或为空。", "newPassword");
+
+
+            User currentUser = DB.User.SingleOrDefault(o => o.Name == userName);
+            if (currentUser == null)
+            {
+                return false;
+            }
+            if (currentUser.Password != oldPassword)
+            {
+                return false;
+            }
+            currentUser.Password = newPassword;
+            return DB.SaveChanges() == 1;
         }
 
-        public bool Modify(SOA.WLIMS.Models.UserModel model)
+        public bool Modify(UserModel model)
         {
-            throw new NotImplementedException();
+            User user = DB.User.SingleOrDefault(o => o.ID == model.ID);
+            if (user != null)
+            {
+                user.AliasName = model.AliasName;
+                user.Email = model.Email;
+                user.Tel = model.Tel;
+                user.Role = model.Role;
+                user.EmployeCode = model.EmployeCode;
+                user.IsDeleted = model.IsDeleted;
+                user.Enable = model.Enable;
+            }
+            return DB.SaveChanges() == 1;
         }
-
         public bool Delete(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                DB.User.SingleOrDefault(o => o.ID == id).IsDeleted = true;
+                return DB.SaveChanges() == 1;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public UserModel Get(int id)
+        {
+            return DB.User.Select(model => new UserModel()
+            {
+                ID = model.ID,
+                Name = model.Name,
+                AliasName = model.AliasName,
+                Email = model.Email,
+                Password = model.Password,
+                Role = model.Role,
+                EmployeCode = model.EmployeCode,
+                Enable = model.Enable,
+                IsDeleted = model.IsDeleted
+            }).SingleOrDefault(o => o.ID == id);
         }
 
-        public SOA.WLIMS.Models.UserModel Get(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<SOA.WLIMS.Models.UserModel> Query(SOA.WLIMS.Models.QueryParam param)
-        {
-            throw new NotImplementedException();
-        }
 
 
-        public Models.UserModel GetUserByName(string userName)
+        public UserModel GetUserByName(string userName)
         {
-            throw new NotImplementedException();
+            return DB.User.Select(model => new UserModel()
+            {
+                ID = model.ID,
+                Name = model.Name,
+                AliasName = model.AliasName,
+                Email = model.Email,
+                Password = model.Password,
+                Role = model.Role,
+                EmployeCode = model.EmployeCode,
+                Enable = model.Enable,
+                IsDeleted = model.IsDeleted
+            }).SingleOrDefault(o => o.Name == userName);
         }
     }
 }
